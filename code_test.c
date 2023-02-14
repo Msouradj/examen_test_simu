1db121532ebc04880b3261dd5e91f3c9eba6e8f0
souradj <souradj.mounien-dit-ravi@etu.u-paris.fr>
2023-02-14 09:57:04 +0100

    Premier commit

--- code_test.c
+++ code_test.c
@@ -27,7 +27,7 @@ void test_case_sample(void)
    CU_ASSERT_STRING_EQUAL("string #1", "string #2");
 }
 
-/*void max_test_1(void) {
+void max_test_1(void) {
   CU_ASSERT_EQUAL( max(1,2), 2);
   CU_ASSERT_EQUAL( max(2,1), 2);
 }
@@ -40,7 +40,7 @@ void max_test_2(void) {
 
 void max_test_3(void) {
   CU_ASSERT_EQUAL( max(-1,-2), -1);
-}*/
+}
 
 /************* Test Runner Code goes here **************/
 
